62c26cbb23ddefcb5c42c2df50550679c67d7071
izavits <izavits@gmail.com>
2020-01-21 10:15:54 +0200

    Add weighted decision tree example on imbalanced dataset

---
+++ src/weighted_decision_trees.py
@@ -0,0 +1,54 @@
+# Class-weighted decision tree on imbalanced dataset
+
+from numpy import mean
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+
+# Generate a synthetic dataset
+print('Generating dataset..')
+X, y = make_classification(n_samples=10000, n_features=2, n_redundant=0,
+                           n_clusters_per_class=1, weights=[0.99], flip_y=0, random_state=2)
+
+print('Running model with the given weights..')
+# Define the model weights and the model
+# The model weights are defined based on the class distribution
+# on the datasetn in this example.
+weights = {0:0.01, 1:1.0}
+model = DecisionTreeClassifier(class_weight=weights)
+
+# Evaluation:
+# Repeated cross-validation, with three repeats of 10-fold cross-validation
+# and the model performance will be reported using the mean ROC area under the
+# curve (ROC AUC) averaged over all repeats and folds.
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+scores = cross_val_score(model, X, y, scoring='roc_auc', cv=cv, n_jobs=-1)
+print('Mean ROC AUC: %.3f' % mean(scores))
+print('')
+
+# Train another model by using grid search to search for weights
+# define model
+print('Running model based on grid search for the weigths..')
+model = DecisionTreeClassifier()
+# define grid
+balance = [{0:100,1:1}, {0:10,1:1}, {0:1,1:1}, {0:1,1:10}, {0:1,1:100}]
+param_grid = dict(class_weight=balance)
+
+# Evaluation
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# Grid search
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=cv, scoring='roc_auc')
+grid_result = grid.fit(X, y)
+
+# Best configuration
+print('Best: %f using %s' % (grid_result.best_score_, grid_result.best_params_))
+print('')
+# Report all
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print('%f (%f) with: %r' % (mean, stdev, param))
+
